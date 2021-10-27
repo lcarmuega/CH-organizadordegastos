452247daadda8f9221c3d56e93c6b775d04d920b
LuCarmuega <lu.carmuega@gmail.com>
2021-10-27 14:43:47 -0300

    new terms of service section

--- js/main.js
+++ js/main.js
@@ -3,14 +3,42 @@ google.charts.load("current", {
 });
 google.charts.setOnLoadCallback(drawChart);
 
-function drawChart() {
+function handleSubmit() {
+  let ingresoTotal = document.getElementById("ingreso").value;
+  let alquiler = document.getElementById("alquiler").value;
+  let servicios = document.getElementById("servicios").value;
+  let ahorros = document.getElementById("ahorros").value;
+  let otros = document.getElementById("otros").value;
+  if ( ingresoTotal == "" || alquiler == "" || servicios == "" || ahorros == "" || otros == "" ) {
+    alert("Es necesario completar todos los campos. ¡Intentá nuevamente!");
+  } else {
+    ingresoTotal = parseInt(ingresoTotal);
+    alquiler = parseInt(alquiler);
+    servicios = parseInt(servicios);
+    ahorros = parseInt(ahorros);
+    otros = parseInt(otros);
+    if ( ingresoTotal < 0 || alquiler < 0 || servicios < 0 || ahorros < 0 || otros < 0 ) {
+      alert( "Los valores ingresados no pueden ser negativos. ¡Intentá nuevamente!" );
+    } else {
+      let resto = ingresoTotal - alquiler - servicios - ahorros - otros;
+      if ( resto < 0 ){
+        alert( "Los gastos ingresados no pueden superar el ingreso. ¡Intentá nuevamente!");
+      } else {
+        drawChart(alquiler, servicios, ahorros, otros, resto)
+      }
+    }
+  }
+  
+}
+
+function drawChart(alquiler, servicios, ahorros, otros, resto) {
   var data = google.visualization.arrayToDataTable([
     ["Gasto", "$"],
-    ["Alquiler", 11],
-    ["Servicios", 2],
-    ["Ahorros", 2],
-    ["Otros", 2],
-    ["Resto", 7],
+    ["Alquiler", alquiler],
+    ["Servicios", servicios],
+    ["Ahorros", ahorros],
+    ["Otros", otros],
+    ["Resto", resto],
   ]);
 
   var options = {
